c226e476ac792324f8da711c0a839ea709ece9bc
Vladimir <gonchar.vladimir@yahoo.com>
2019-12-03 10:47:24 +0300

    Reading tags for state, transition, dep by the name of the PFR Maintenance Manager block has been added

--- src/ru/pfr/prosess/StaxStreamProcessor.java
+++ src/ru/pfr/prosess/StaxStreamProcessor.java
@@ -14,6 +14,9 @@ public class StaxStreamProcessor {
 
         String urlString;
         boolean bossTOPfr=false;
+        boolean isTransition=false;
+        boolean isDep=false;
+
         urlString = "file:///D:/IdeaProject/readProcess/in/%D0%9D%D0%92%D0%9F_10_%D0%94%D0%9D%D0%95%D0%99_%D0%9A%D0%A2%D0%9B_3_0_0_2019.09.13/processDefinition.xml";
         System.out.println("Using " + urlString);
 
@@ -29,52 +32,56 @@ public class StaxStreamProcessor {
         XMLInputFactory factory = XMLInputFactory.newInstance();
         XMLStreamReader parser = factory.createXMLStreamReader(in);
         while (parser.hasNext()) {
-//            System.out.println("идет процесс анализа XML");
             int event = parser.next();
             if (event == XMLStreamConstants.START_ELEMENT) {
                 if (parser.getLocalName().equals("state") || parser.getLocalName().equals("transition") || parser.getLocalName().equals("dep")) {
                     String name = parser.getAttributeValue(null, "name");
                     String to = parser.getAttributeValue(null, "to");
-//                    String dep = parser.getElementText();
+                    String deptype = parser.getAttributeValue(null, "type");
 
                     if (name != null) {
+
                         if (name.equals("Руководитель ТО ПФР")) {
                             System.out.println("namne " + name);
+
                             bossTOPfr=true;
-//                            System.out.println("to " + to);
-                        }else {
-                            bossTOPfr=false;
+
                         }
                         if (bossTOPfr){
-                            System.out.println("to " + to);
+                            if (to!=null){
+                                System.out.println("to " + to);
+                                isTransition=true;
+                            }
 
                         }
-//                        System.out.println("dep " + dep);
+
+                     }
+                    if (deptype!=null){
+                        if (isTransition){
+                            System.out.println(deptype);
+                        }
 
                     }
-//                        if (to.equals("Руководитель ТО ПФР")){
-//                            System.out.println("namne "+name);
-////                            System.out.println("to "+to);
-//                        }
-//                            while(parser.hasNext()){
-//
-//                                int ventch=parser.next();
-//                                if (event == XMLStreamConstants.START_ELEMENT){
-//                                    System.out.println(parser.getName());
-////                                    if (parser.getLocalName().equals("transition")) {
-//////                                        String to = parser.getAttributeValue(null, "to");
-//////                                        if (name != null){
-//////                                            if (to.equals("")){
-//////                                                System.out.println("to "+to);
-//////                                            }
-//////                                        }
-////                                    }
-//                                }
-//                             }
-//                        System.out.println("namne "+name);
-//                        System.out.println("to "+to);
+
                 }
             }
+            if (event == XMLStreamConstants.END_ELEMENT){
+                if (parser.getLocalName().equals("state")) {
+                    bossTOPfr=false;
+                }
+            }
+            if (event == XMLStreamConstants.END_ELEMENT) {
+                if (parser.getLocalName().equals("transition")) {
+                    isTransition = false;
+                }
+            }
+
+            if (event == XMLStreamConstants.END_ELEMENT) {
+                if (parser.getLocalName().equals("dep")) {
+                    isDep = false;
+                }
+            }
+
         }
         System.out.println("процесс анализа XML завершен");
     }
